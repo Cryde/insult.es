05edd33a84b9cf8163abebf61bbd70c2a97c9ed8
Jérémy <t.jeremy.0@gmail.com>
2016-12-20 22:06:18 +0100

    feature: add doctrine migration from begining

---
+++ app/DoctrineMigrations/Version20161220210345.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161220210345 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE insult (id INT AUTO_INCREMENT NOT NULL, insult VARCHAR(255) NOT NULL, insult_canonical VARCHAR(255) NOT NULL, date_post DATETIME NOT NULL, UNIQUE INDEX UNIQ_DAB025AF47EF594 (insult_canonical), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE insult');
+    }
+}
