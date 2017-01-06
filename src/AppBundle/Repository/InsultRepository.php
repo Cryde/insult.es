ca764976253576c475ce9b9f7719078adb67fce9
Jérémy <t.jeremy.0@gmail.com>
2017-01-06 22:29:51 +0100

    feature: add a RAND function in DQL to avoid some reccurence in insults

--- src/AppBundle/Repository/InsultRepository.php
+++ src/AppBundle/Repository/InsultRepository.php
@@ -13,23 +13,11 @@ class InsultRepository extends \Doctrine\ORM\EntityRepository
      */
     public function getRandom()
     {
-        $sq = $this
-            ->getEntityManager()
-            ->createQuery('SELECT i FROM AppBundle:Insult i WHERE i.id >= :rand')
-            ->setParameters(['rand' => rand(0, $this->getMaxId())])
-            ->setMaxResults(1);
-
-        return $sq->getSingleResult();
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getMaxId()
-    {
-        return $this
-            ->getEntityManager()
-            ->createQuery('SELECT MAX(i.id) FROM AppBundle:Insult i')
-            ->getSingleScalarResult();
+        return  $this->createQueryBuilder('i')
+            ->addSelect('RAND() as HIDDEN rand')
+            ->addOrderBy('rand')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleResult();
     }
 }
