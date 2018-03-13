971cde9020a25027ec198dd0813826564ceba7ef
Jérémy <t.jeremy.0@gmail.com>
2018-03-13 07:53:12 +0100

    fix: issue with missing features

--- deploy.php
+++ deploy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/symfony4.php';
@@ -22,45 +23,63 @@ add('writable_dirs', []);
 // Hosts
 inventory('hosts.yml');
 
-set('env', function () {
-    return [
-        'APP_ENV' => get('APP_ENV'),
-        'DATABASE_URL' => get('DATABASE_URL')
-    ];
-});
+set(
+    'env',
+    function () {
+        return [
+            'APP_ENV'      => get('APP_ENV'),
+            'DATABASE_URL' => get('DATABASE_URL'),
+        ];
+    }
+);
 
 // Tasks
 desc('Build Brunch assets');
-task('assets:build', function() {
-    run('cd {{release_path}} && {{bin/npm}} run build');
-});
+task(
+    'assets:build',
+    function () {
+        run('cd {{release_path}} && {{bin/npm}} run build');
+    }
+);
 after('npm:install', 'assets:build');
 
 desc('Remove node_modules folder');
-task('assets:clean', function() {
-    run('cd {{release_path}} && rm -rf node_modules');
-});
+task(
+    'assets:clean',
+    function () {
+        run('cd {{release_path}} && rm -rf node_modules');
+    }
+);
 after('deploy:symlink', 'assets:clean');
 
 desc('Restart PHP-FPM service');
-task('php-fpm:restart', function() {
-    run('sudo service php7.2-fpm reload');
-});
+task(
+    'php-fpm:restart',
+    function () {
+        run('sudo service php7.2-fpm reload');
+    }
+);
 after('cleanup', 'php-fpm:restart');
 
 
-task('deploy:cache:clear', function () {
-	    run('{{bin/php}} {{bin/console}} cache:clear --no-warmup');
-})->desc('Clear cache');
+task(
+    'deploy:cache:clear',
+    function () {
+        run('{{bin/php}} {{bin/console}} cache:clear --no-warmup');
+    }
+)->desc('Clear cache');
 
-task('deploy:cache:warmup', function () {
-	    run('{{bin/php}} {{bin/console}} cache:warmup');
-})->desc('Warm up cache');
+task(
+    'deploy:cache:warmup',
+    function () {
+        run('{{bin/php}} {{bin/console}} cache:warmup');
+    }
+)->desc('Warm up cache');
 before('deploy:symlink', 'deploy:cache:clear');
 
 after('deploy:cache:clear', 'deploy:cache:warmup');
 
 
 // Migrate database before symlink new release.
-//before('deploy:symlink', 'database:migrate');
+before('deploy:symlink', 'database:migrate');
 after('deploy:update_code', 'npm:install');
