bc5618618094248100c091fd62b18ca444cdf9b5
Jérémy <jtonneau@thefabrik.io>
2019-11-22 07:45:27 +0100

    chore: update php libs

--- src/Repository/InsultRepository.php
+++ src/Repository/InsultRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Insult;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * InsultRepository
@@ -15,9 +15,9 @@ class InsultRepository extends ServiceEntityRepository
     /**
      * InsultRepository constructor.
      *
-     * @param RegistryInterface $registry
+     * @param ManagerRegistry $registry
      */
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Insult::class);
     }
--- src/Repository/InsultVoteRepository.php
+++ src/Repository/InsultVoteRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\InsultVote;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * Class InsultVoteRepository
@@ -17,9 +17,9 @@ class InsultVoteRepository extends ServiceEntityRepository
     /**
      * InsultRepository constructor.
      *
-     * @param RegistryInterface $registry
+     * @param ManagerRegistry $registry
      */
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, InsultVote::class);
     }
